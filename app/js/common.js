86e9d24920cb8613522cded4c5bfa26caf2a1a4d
Alex Kozlov <kau88@rambler.ru>
2018-11-21 17:15:56 +0200

    made profile info

--- app/js/common.js
+++ app/js/common.js
@@ -4,4 +4,19 @@ $( document ).ready(function() {
   	$('.aside').toggleClass('aside__active');
   });
 
+
+
+
+
+$(function () {
+  $('[data-toggle="tooltip-pink"]').tooltip({
+  	template: '<div class="tooltip pink" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
+  });
+});
+
+$(function () {
+  $('[data-toggle="tooltip-blue"]').tooltip({
+  	template: '<div class="tooltip blue" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
+  });
+});
 })//end document.ready function
